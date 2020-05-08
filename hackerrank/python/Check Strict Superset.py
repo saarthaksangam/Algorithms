d0b2f9fa5e0a0de6448a1f088ef508e346c3a4a7
Saarthak Sangamnerkar <31529049+saarthaksangam@users.noreply.github.com>
2020-05-08 19:33:09 +0530

    Update Check Strict Superset.py

--- hackerrank/python/Check Strict Superset.py
+++ hackerrank/python/Check Strict Superset.py
@@ -1,9 +1,4 @@
 # https://www.hackerrank.com/challenges/py-check-strict-superset
-
-A = set(map(int, input().split()))
-for _ in range(int(input())):
-    X = set(map(int, input().split()))
-    if A.issuperset(X) != True or len(A) == len(X): 
-        print(False)
-        break 
-else: print(True)
+#using generator
+a = set(input().split())
+print(all(a > set(input().split()) for _ in range(int(input()))))
